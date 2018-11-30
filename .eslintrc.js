31f85591dbba31b8fb6b81206d3b9c831f0bc67d
109 <1515885616@qq.com>
2018-11-30 17:33:42 +0800

    register

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0,
     'no-tabs': 'off',
-    'indent': 'off',
+    'indent': 'off'
   }
 }
