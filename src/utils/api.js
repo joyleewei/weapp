77f1b11f4f473721e5d0606c216136447daf3dee
109 <1515885616@qq.com>
2018-12-01 11:35:54 +0800

    更新用户头像

--- src/utils/api.js
+++ src/utils/api.js
@@ -138,10 +138,33 @@ const logout = async(params = {}) => {
     return logoutResponse
 }
 
+const updateFile = async (options = {}) => {
+    // 显示 loading
+    wepy.showLoading({title: '上传中'})
+    // 获取 token
+    let accessToken = await getToken()
+
+    // 拼接url
+    options.url = host + '/' + options.url
+    let header = options.header || {}
+    // 将token 设置在header 中
+    header.Authorization = 'Bearer ' + accessToken
+    options.header = header
+
+    // 上传文件
+    let response = await wepy.uploadFile(options)
+
+    // 隐藏 loading
+    wepy.hideLoading()
+
+    return response
+}
+
 export default {
     request,
     authRequest,
     refreshToken,
     login,
-    logout
+    logout,
+    updateFile
 }
